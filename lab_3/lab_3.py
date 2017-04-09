051825cf6db892f5598e130bd8da73d693d25611
Edward Fan <edwardzfan@gmail.com>
2017-04-09 00:26:05 -0400

    Lab 3 (preliminary)

---
+++ lab_3/lab_3.py
@@ -0,0 +1,85 @@
+import numpy as np
+from gpkit import Variable, Model
+
+# Constants
+# b = Variable("b", 1.52, "m", "wing span")
+c_d0 = Variable("c_d0", 0.02, "-", "drag coefficient 0")
+c_d1 = Variable("c_d1", -0.004, "-", "drag coefficient 1")
+c_d2 = Variable("c_d2", 0.02, "-", "drag coefficient 2")
+c_d8 = Variable("c_d8", 1.0, "-", "drag coefficient 8")
+c_l0 = Variable("c_l0", 0.8, "-", "lift coefficient 0")
+CDA_0 = Variable("CDA_0", 0.004, "m^2", "fuselage drag area")
+e = Variable("e", 0.95, "-", "efficiency factor")
+eps = Variable("\\epsilon", 0.03, "-", "airfoil camber ratio")
+E_foam = Variable("E_{foam}", 19.3e6, "Pa", "Young's modulus of foam")
+g = Variable("g", 9.81, "m/s^2", "acceleration due to gravity")
+N = Variable("N", 1.0, "-", "load factor")
+rho_air = Variable("\\rho_{air}", 1.225, "kg/m^3", "density of air")
+rho_foam = Variable("\\rho_{foam}", 32.0, "kg/m^3", "density of foam")
+R = Variable("R", 12.5, "m", "turning radius")
+tau = Variable("\\tau", 0.11, "-", "airfoil thickness ratio")
+T_max = Variable("T_{max}", 0.7, "N", "maximum thrust")
+W_fuse = Variable("W_{fuse}", 2.7, "N", "fuselage weight")
+
+constraints = []
+
+# Free variables
+C_L = Variable("C_L", "-", "lift coefficient")
+S = Variable("S", "m^2", "planform area")
+AR = Variable("AR", "-", "aspect ratio")
+#lamb = Variable("lamb", "-", "taper ratio")
+p1 = Variable("p1", "-", "2*lambda + 1")
+p2 = Variable("p2", "-", "lambda + 1")
+constraints += [2*p1 >= 1 + p2]
+p3 = Variable("p3", "-", "C_L - c_l0")
+constraints += [C_L >= 0.8 + p3]
+p4 = Variable("p4", "-", "lambda - 1")
+constraints += [p2 >= p4 + 2]
+
+# Dependent variables
+
+b = Variable("b", "m", "wing span")
+constraints += [b >= (AR * S)**0.5]
+
+c_r = Variable("c_r", "m", "root chord")
+constraints += [c_r >= 2 * (AR * S)**0.5 / (AR * p2)]
+
+C_DP = Variable("C_DP", "-", "wing drag coefficient")
+constraints += [C_DP >= c_d0 + c_d1 * p3 + c_d2 * p3**2 + \
+                c_d8 * p3**8]
+
+C_Di = Variable("C_Di", "-", "induced drag coefficient")
+constraints += [C_Di >= C_L**2 / (np.pi * AR * e)]
+
+C_D = Variable("C_D", "-", "drag coefficient")
+constraints += [C_D >= CDA_0 / S + C_DP + C_Di]
+
+W_max = Variable("W_max", "N", "total supportable weight")
+constraints += [W_max <= T_max * C_L / C_D]
+
+W_wing = Variable("W_wing", "N", "wing weight")
+constraints += [W_wing >= 1.2 * b * tau * rho_foam * g * c_r**2 * \
+                (1.0/6 * p4**2 + 0.5 * p4 + 0.5)]
+
+W_pay = Variable("W_pay", "N", "payload weight")
+constraints += [W_pay + W_wing + W_fuse <= W_max]
+
+p5 = Variable("p5", "N", "fuselage + payload weight")
+constraints += [p5 >= W_fuse + W_pay]
+
+p6 = Variable("p6", "-", "tau^2 + eps^2")
+constraints += [p6 >= tau**2 + eps**2]
+
+delta_over_b = Variable("delta_over_b", "-", "deflection/span ratio")
+constraints += [delta_over_b >= 0.018 * N * p5 / \
+                (E_foam * tau * p6) * p2**3 * \
+                p1 * AR**3 / S]
+
+# Constraints
+constraints += [delta_over_b <= 0.15]
+
+# Results
+m = Model(1.0/W_pay, constraints)
+sol = m.solve(verbosity=0)
+print(sol.summary())
+
---
+++ lab_3/lab_3_old.py
@@ -0,0 +1,429 @@
+#! /usr/bin/env python3
+
+import math
+import numpy as np
+from scipy.optimize import minimize_scalar, brute
+
+import matplotlib.pyplot as plt
+
+class Plane():
+    """
+    Variable convention:
+    Parameters are stored in the object using their names (listed below).
+    A method to calculate a parameter has "c_" appended to the start of the
+    parameter name. These methods set the result into the Plane object. These 
+    methods do not take any arguments; they read from the Plane object.
+
+    Ex. Calculating and setting W_wing:
+    p = Plane(...various required parameters)
+    p.c_W_wing() # Calculates W_wing and sets it in p.
+    print(p.W_wing) # The value of W_wing.
+
+    Parameter names:
+    a: something for c_l
+    A: cross-sectional area
+    b: wing span
+    c: average wing chord
+    c_d0...c_d8: XFOIL-computed polars
+    c_l: coefficient of airfoil lift (also c_l0 from XFOIL)
+    c_r: root wing chord
+    c_t: tip wing chord
+    C_D: coefficient of drag
+    C_Dfuse: coefficient of fuselage drag
+    C_Di: coefficient of induced drag
+    C_DP: coefficient of profile drag on wing
+    CDA_0: fuselage drag
+    delta: tip deflection
+    delta_over_b: deflection/span ratio
+    delta_over_b_max: maximum allowable deflection/span ratio
+    e: span efficiency factor
+    eps: airfoil camber ratio
+    E_foam: foam Young's modulus (stiffness)
+    g: acceleration due to gravity
+    lamb: taper ratio (not lambda because of Python keyword)
+    mu_air: dynamic viscosity of air
+    N: load factor
+    rho_air: air density
+    rho_foam: foam density
+    R: radius of circle
+    Re: Reynolds number
+    Re_ref: Reference Reynolds number
+    tau: airfoil thickness ratio
+    t_rev: time to complete one revolution
+    T: thrust available
+    T_max: maximum available thrust
+    T_req: required thrust to match t_rev
+    V: flight speed
+    W: total weight
+    W_max: maximum total weight (from T_max)
+    W_supportable: maximum total weight (from T)
+    W_pay: weight of payload
+    W_pay_max: maximum payload weight supportable
+    W_fuse: weight of fuselage
+    W_wing: weight of wing
+    ---------
+    AR: aspect ratio
+    S: reference (wing) area
+    C_L: coefficient of lift
+    """
+    
+    def __init__(self, **kwargs):
+        """
+        Initialize plane with chosen variables.
+
+        Args: Takes any number of keyword (named) arguments.
+        Returns: New Plane object with given attributes set.
+        """
+        
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def check_vars(self, to_calc, required_vars):
+        """
+        Checks that a given calculation is currently possible, based on which
+        variables have been defined in the Plane object. This function should
+        be called prior to calculating any parameter.
+
+        Args: to_calc: String; name of the parameter to be calculated (for 
+                       debugging purposes).
+              required_vars: Tuple; names of required variables in this 
+                             calculation.
+        Returns: True if calculation is valid; raises AttributeError otherwise.
+        """
+
+        all_vars_valid = True
+
+        for r_v in required_vars:
+            if r_v not in self.__dict__.keys():
+                print("Cannot calculate", to_calc, "- missing", r_v)
+                all_vars_valid = False
+
+        if not all_vars_valid:
+            raise AttributeError
+
+        return all_vars_valid # True
+
+    def c_A(self):
+        required_vars = ['c', 'tau']
+        self.check_vars('A', required_vars)
+        
+        self.A = 0.6 * (self.c)**2 * self.tau
+
+    def c_AR(self):
+        required_vars = ['b', 'c']
+        self.check_vars('AR', required_vars)
+
+        self.AR = self.b / self.c
+
+    def c_b(self):
+        required_vars = ['AR', 'S']
+        self.check_vars('b', required_vars)
+
+        self.b = (self.AR * self.S)**0.5
+        
+    def c_c(self):
+        required_vars = ['S', 'AR']
+        self.check_vars('c', required_vars)
+        
+        self.c = (self.S / self.AR)**0.5
+
+    def c_c_r(self):
+        required_vars = ['AR', 'b', 'lamb']
+        self.check_vars('c_r', required_vars)
+
+        self.c_r = 2 * self.b / (self.AR * (self.lamb + 1)) 
+        
+    def c_C_D(self):
+        required_vars = ['S', 'C_DP', 'C_Di']
+        self.check_vars('C_D', required_vars)
+
+        self.CDA_0 = 0.002 + 0.002 * self.S / 0.228
+        
+        self.C_D = self.CDA_0 / self.S + self.C_DP + self.C_Di
+            
+    def c_C_DP(self):
+        required_vars = ['c_l', 'Re', 'Re_ref', 'a', 'tau']
+        self.check_vars('C_DP', required_vars)
+
+        self.c_d0 = 0.02 * (1 + self.tau ** 2)
+        self.c_d1 = -0.005 / (1 + 6.0 * self.tau)
+        self.c_d2 = 0.16 * (1 + 60.0 * self.tau)
+        self.c_d8 = 1.0
+        self.c_l0 = 1.25 - 3.0 * self.tau
+        
+        lift_diff = self.c_l - self.c_l0
+        self.C_DP = (self.c_d0 + self.c_d1 * lift_diff + self.c_d2 * lift_diff**2 + self.c_d8 * lift_diff**8) * (self.Re / self.Re_ref)**(self.a)
+        
+    def c_C_Di(self):
+        required_vars = ['C_L', 'AR', 'e']
+        self.check_vars('C_Di', required_vars)
+
+        self.C_Di = (self.C_L)**2 / (math.pi * self.AR * self.e)
+        
+    def c_c_l(self):
+        required_vars = ['C_L']
+        self.check_vars('c_l', required_vars)
+
+        self.c_l = self.C_L
+
+    def c_delta_over_b(self):
+        required_vars = ['N', 'W_fuse', 'W_pay', 'E_foam', 'tau', 'eps', 'lamb', 'AR', 'S']
+        self.check_vars('delta_over_b', required_vars)
+
+        self.delta_over_b = 0.018 * self.N * \
+            (self.W_fuse + self.W_pay) / (self.E_foam * self.tau * ((self.tau)**2 + 0.7*(self.eps)**2)) * \
+            (1.0 + self.lamb)**3 * (1.0 + 2.0 * self.lamb) * (self.AR)**3 / self.S
+
+    def c_eps(self):
+        required_vars = ['tau']
+        self.check_vars('tau', required_vars)
+
+        self.eps = 0.1 - 0.5 * self.tau
+
+    def c_lamb(self):
+        required_vars = ['c_t', 'c_r']
+        self.check_vars('lamb', required_vars)
+
+        self.lamb = self.c_t / self.c_r
+
+    def c_N(self, ignore_int=False):
+        required_vars = ['W_fuse', 'W_wing', 'W_pay', 'rho_air', 'g', 'R', 'S', 'C_L']
+        self.check_vars('N', required_vars)
+
+        intermediate = (1 - ((self.W_fuse + self.W_wing + self.W_pay) / \
+            (0.5 * self.rho_air * self.g * self.R * self.S * self.C_L))**2)
+        
+        if ignore_int:
+            intermediate = abs(intermediate)
+        else:
+            if intermediate < 0:
+                raise ValueError # Imaginary result produced otherwise.
+
+        self.N = intermediate**-0.5
+
+    def c_Re(self):
+        required_vars = ['rho_air', 'V', 'c', 'mu_air']
+        self.check_vars('Re', required_vars)
+
+        self.Re = self.rho_air * self.V * self.c / self.mu_air
+
+    def c_S(self):
+        required_vars = ['b', 'c']
+        self.check_vars('S', required_vars)
+
+        self.S = self.b * self.c
+
+    def c_t_rev(self):
+        required_vars = ['R', 'V']
+        self.check_vars('t_rev', required_vars)
+
+        self.t_rev = 2 * math.pi * self.R / self.V
+
+    def c_T_req(self):
+        required_vars = ['rho_air', 'V', 'S', 'C_D']
+        self.check_vars('t_rev', required_vars)
+
+        self.T_req = 0.5 * self.rho_air * (self.V)**2 * self.S * self.C_D
+
+    def c_T(self):
+        required_vars = ['N', 'W', 'C_D', 'C_L']
+        self.check_vars('T', required_vars)
+
+        self.T = self.N * self.W * self.C_D / self.C_L
+
+    def c_T_max(self):
+        required_vars = ['V']
+        self.check_vars('T_max', required_vars)
+
+        self.T_max = 1.0 - 0.08 * self.V - 0.0028 * self.V**2
+
+    def c_V(self, ignore_int=False):
+        required_vars = ['g', 'R', 'N']
+        self.check_vars('V', required_vars)
+
+        intermediate = (self.N)**2 - 1
+
+        if ignore_int:
+            intermediate = abs(intermediate)
+        else:
+            if intermediate < 0:
+                raise ValueError # Imaginary result produced otherwise.
+
+        self.V = (self.g * self.R * (intermediate)**0.5)**0.5 
+
+    def c_W(self):
+        required_vars = ['W_wing', 'W_fuse', 'W_pay']
+        self.check_vars('W', required_vars)
+
+        self.W = self.W_wing + self.W_fuse + self.W_pay
+
+    def c_W_max(self):
+        required_vars = ['T_max', 'C_D', 'C_L']
+        self.check_vars('W_max', required_vars)
+
+        self.W_max = self.T_max * self.C_L / self.C_D
+
+    def c_W_supportable(self):
+        required_vars = ['T', 'C_D', 'C_L']
+        self.check_vars('W_supportable', required_vars)
+
+        self.W_supportable = self.T * self.C_L / self.C_D
+
+    def c_W_pay_max(self):
+        required_vars = ['W_max', 'W_wing', 'W_fuse']
+        self.check_vars('W_pay_max', required_vars)
+
+        self.W_pay_max = self.W_max - self.W_wing - self.W_fuse
+
+    def c_W_wing(self):
+        required_vars = ['rho_foam', 'g', 'tau', 'b', 'c_r', 'lamb']
+        self.check_vars('W_wing', required_vars)
+
+        lmo = self.lamb - 1.0
+
+        self.W_wing = 1.2 * self.b * self.tau * self.rho_foam * self.g * (self.c_r)**2 * \
+                      (1.0/6 * lmo**2 + 0.5 * lmo + 0.5)
+                                                                          
+        
+
+# Represent Plane Vanilla parameters as a dictionary, which can then be turned
+# into a Plane object.
+plane_vanilla_params = {
+    'a': -0.75,
+    'b': 1.52,
+    'c_r': 0.2,
+    'c_t': 0.1,
+    'CDA_0': 0.004,
+    'delta': 0.18,
+    'e': 0.95,
+    'E_foam': 19.3e6,
+    'g': 9.81,
+    'mu_air': 1.81e-5,
+    'N': 1, # maximum load factor
+    'rho_air': 1.225,
+    'rho_foam': 32.0,
+    'R': 12.5,
+    'Re_ref': 100000,
+    'T_max': 0.7,
+    'W_fuse': 2.7
+}
+
+# Calculating W_pay_max for Plane Vanilla
+p = Plane(**plane_vanilla_params)
+p.c_lamb()
+
+# Optimize combined objective function
+
+def optimized_combined(args, delta_over_b_max, tau, print_result=False, ignore_db=False):
+    # args:
+    # 0: S
+    # 1: AR
+    # 2: C_L (loaded)
+    # 3: W_pay
+    # 4: tau
+    
+    try:
+        p.S = args[0]
+        p.AR = args[1]
+        p.C_L = args[2]
+        p.W_pay = args[4]
+        p.tau = tau
+        p.c_b()
+        p.c_c()
+        p.c_A()
+        p.c_W_wing()
+        p.c_W()
+        p.c_N(True)
+        p.c_V(True)
+        p.c_Re()
+        p.c_eps()
+        p.c_c_r()
+        p.c_c_l()
+        p.c_C_DP()
+        p.c_C_Di()
+        p.c_C_D()
+
+        p.c_t_rev()
+        t_loaded = p.t_rev
+        p.c_delta_over_b()
+        p.c_T_req()
+        p.c_T_max()
+        p.c_T()
+        p.c_W_max()
+
+        if not ignore_db and p.delta_over_b > delta_over_b_max:
+            raise ValueError
+        
+        if not ignore_db and p.T_req > p.T_max:
+            raise ValueError
+
+        if not ignore_db and p.W > p.W_max:
+            raise ValueError
+
+        if print_result:
+            print('For {}:'.format(delta_over_b_max))
+            print('t_rev_min', p.t_rev)
+            print('C_D', p.C_D)
+            print('d/b', p.delta_over_b)
+            print('V', p.V)
+            print('T', p.T)
+            print('T_req', p.T_req)
+            print('T_max', p.T_max)
+            print('N', p.N)
+            print('\n')
+
+        p.W_pay = 0
+        p.C_L = args[3]
+        p.c_W()
+        p.c_N(True)
+        p.c_V(True)
+        p.c_Re()
+        p.c_c_l()
+        p.c_C_DP()
+        p.c_C_Di()
+        p.c_C_D()
+        p.c_t_rev()
+        t_empty = p.t_rev
+        p.c_delta_over_b()
+        p.c_T_req()
+        p.c_T_max()
+        p.c_T()
+        p.c_W_max()
+
+        if not ignore_db and p.delta_over_b > delta_over_b_max:
+            raise ValueError
+        
+        if not ignore_db and p.T_req > p.T_max:
+            raise ValueError
+
+        if not ignore_db and p.W > p.W_max:
+            raise ValueError
+
+        if print_result:
+            print('For {}:'.format(delta_over_b_max))
+            print('t_rev_min', p.t_rev)
+            print('C_D', p.C_D)
+            print('d/b', p.delta_over_b)
+            print('V', p.V)
+            print('T', p.T)
+            print('T_req', p.T_req)
+            print('T_max', p.T_max)
+            print('N', p.N)
+            print('\n')
+
+        print('Final result:', args[3] / (t_loaded + t_empty))
+
+        return -1.0 * args[3] / (t_loaded + t_empty)
+    
+    except ValueError:
+        return 10000
+
+ranges = (slice(0.01, 0.2, 0.01), slice(2, 15, .25),
+          slice(0.3, 1.01, 0.05), slice(0.3, 1.01, 0.05),
+          slice(.5, 5, .1),)
+
+tmp = brute(optimized_combined, ranges, args=(0.15, 0.11), finish=None)
+print('Combined (constrained for d/b <= 0.15):', tmp)
+optimized_combined(tmp, 0.15, True)
+
+
